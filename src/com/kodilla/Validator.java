6e5682e6d2dbdc875eeebd90b3bf69f0f687459e
KrzysztofZakrocki <zakrocki.krzysztof@gmail.com>
2019-10-31 23:54:51 +0100

    Tic Tac Toe v1.0

--- src/com/kodilla/Validator.java
+++ src/com/kodilla/Validator.java
@@ -3,77 +3,40 @@ package com.kodilla;
 public class Validator {
 
     public boolean checkDidYouWin(Shape[] gameBoard) {
-        if (checkFirstRow(gameBoard) == true || checkSecondRow(gameBoard) == true || checkThirdRow(gameBoard) == true ||
+        return checkFirstRow(gameBoard) == true || checkSecondRow(gameBoard) == true || checkThirdRow(gameBoard) == true ||
                 checkFirstCol(gameBoard) == true || checkSecondCol(gameBoard) == true || checkThirdCol(gameBoard) == true ||
-                checkFirstSlant(gameBoard) == true || checkSecondSlant(gameBoard) == true) {
-            return true;
-        } else {
-            return false;
-        }
+                checkFirstSlant(gameBoard) == true || checkSecondSlant(gameBoard) == true;
     }
 
-
     private Boolean checkFirstRow(Shape[] gameBoard) {
-        if(gameBoard[0] == gameBoard[1] && gameBoard[1] == gameBoard[2] && gameBoard[0] != null) {
-            return true;
-        }else {
-            return false;
-        }
+        return gameBoard[0] == gameBoard[1] && gameBoard[1] == gameBoard[2] && gameBoard[0] != null;
     }
 
     private Boolean checkSecondRow(Shape[] gameBoard) {
-        if(gameBoard[3] == gameBoard[4] && gameBoard[3] == gameBoard[5] && gameBoard[3] != null) {
-            return true;
-        }else {
-            return false;
-        }
+        return gameBoard[3] == gameBoard[4] && gameBoard[3] == gameBoard[5] && gameBoard[3] != null;
     }
 
     private Boolean checkThirdRow(Shape[] gameBoard) {
-        if(gameBoard[6] == gameBoard[7] && gameBoard[6] == gameBoard[8] && gameBoard[6] != null) {
-            return true;
-        }else {
-            return false;
-        }
+        return gameBoard[6] == gameBoard[7] && gameBoard[6] == gameBoard[8] && gameBoard[6] != null;
     }
 
     private Boolean checkFirstCol(Shape[] gameBoard) {
-        if(gameBoard[0] == gameBoard[3] && gameBoard[0] == gameBoard[6] && gameBoard[0] != null) {
-            return true;
-        }else {
-            return false;
-        }
+        return gameBoard[0] == gameBoard[3] && gameBoard[0] == gameBoard[6] && gameBoard[0] != null;
     }
 
     private Boolean checkSecondCol(Shape[] gameBoard) {
-        if(gameBoard[1] == gameBoard[4] && gameBoard[1] == gameBoard[7] && gameBoard[1] != null) {
-            return true;
-        }else {
-            return false;
-        }
+        return gameBoard[1] == gameBoard[4] && gameBoard[1] == gameBoard[7] && gameBoard[1] != null;
     }
 
     private Boolean checkThirdCol(Shape[] gameBoard) {
-        if(gameBoard[2] == gameBoard[5] && gameBoard[2] == gameBoard[8] && gameBoard[2] != null) {
-            return true;
-        }else {
-            return false;
-        }
+        return gameBoard[2] == gameBoard[5] && gameBoard[2] == gameBoard[8] && gameBoard[2] != null;
     }
 
     private Boolean checkFirstSlant(Shape[] gameBoard) {
-        if(gameBoard[0] == gameBoard[4] && gameBoard[0] == gameBoard[8] && gameBoard[0] != null) {
-            return true;
-        }else {
-            return false;
-        }
+        return gameBoard[0] == gameBoard[4] && gameBoard[0] == gameBoard[8] && gameBoard[0] != null;
     }
 
     private Boolean checkSecondSlant(Shape[] gameBoard) {
-        if(gameBoard[2] == gameBoard[4] && gameBoard[2] == gameBoard[6] && gameBoard[2] != null) {
-            return true;
-        }else {
-            return false;
-        }
+        return gameBoard[2] == gameBoard[4] && gameBoard[2] == gameBoard[6] && gameBoard[2] != null;
     }
 }
